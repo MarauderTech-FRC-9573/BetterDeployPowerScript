3f2430a46a7f98a02dd5f1c151cd192b577e5dd8
9573maraudertech <162756411+9573maraudertech@users.noreply.github.com>
2024-11-02 12:01:36 -0700

    some reason the gradle command is bugged

--- BetterFRCDeploy.ps1
+++ BetterFRCDeploy.ps1
@@ -1,9 +1,12 @@
 ﻿param([string]$gitBranchName)
+
 netsh wlan connect name="I21WIRELESS"
 Write-Host "Connecting to I21WIRELESS..."
 Start-Sleep -Seconds 1
+
 $connectionStatus = netsh wlan show interfaces | Select-String -Pattern "State" | Select-String -Pattern "connected"
-if ($connectionStatus){
+Write-Host $connectionStatus
+
     cd "C:\Users\ItsYa\Documents\Github\v2-crescendo-2024"
     git switch $gitBranchName
     git pull
@@ -11,11 +14,8 @@ if ($connectionStatus){
     netsh wlan connect name="9573_Joshua"
     Write-Host "Connecting to Joshua..."
     Start-Sleep -Seconds 1
+
     $connectionStatus2 = netsh wlan show interfaces | Select-String -Pattern "State" | Select-String -Pattern "connected"
-    
-    if ($connectionStatus2){
+    Write-Host $connectionStatus2
 
-        cmd.exe /d /c gradlew deploy  -PteamNumber=9573 --offline  -Dorg.gradle.java.home="C:\Users\Public\wpilib\2024\jdk"
-
-    } else {Write-Host "Connection to Joshua failed."}
-} else {Write-Host "Connection to I21WIRELESS failed."}
+    cmd.exe /d /c gradlew deploy  -PteamNumber=9573 --offline  -Dorg.gradle.java.home="C:\Users\Public\wpilib\2024\jdk"
